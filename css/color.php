5a4359f56be3d3dcc4cd82ec7e0b68c783cd494e
Ricain <jeanmercadier@gmail.com>
2014-01-01 22:13:20 +0100

    re mod couleur

--- css/color.php
+++ css/color.php
@@ -5,6 +5,7 @@
     require_once '../login.inc';
     require_once '../lib/sql.php';
     require_once '../lib/bibli.php';
+
 	header('Content-type: text/css');
 
 	$inter = hex2rgb(selectCouleur($_SESSION["user_id"]));
@@ -29,6 +30,17 @@
 		return "rgb(" . $ro . "," . $go . "," . $bo . ")";
 	}
 
+    function darck2(){
+        global $r,$g,$b;
+        $ro = $r-60;
+        $go = $g-60;
+        $bo = $b-60;
+        if ($ro<0) { $ro = 0; }
+        if ($go<0) { $go = 0; }
+        if ($bo<0) { $bo = 0; }
+        return "rgb(" . $ro . "," . $go . "," . $bo . ")";
+    }
+
     function light(){
         global $r,$g,$b;
         $ro = $r+30;
@@ -40,40 +52,27 @@
         return "rgb(" . $ro . "," . $go . "," . $bo . ")";
     }
 
-    function blanc(){
-        global $r,$g,$b;
-        $ro = intval($r*1200/255);
-        $go = intval($g*1200/255);
-        $bo = intval($b*1200/255);
-        return "rgb(" . $ro . "," . $go . "," . $bo . ")";
-    }
-
-    $default = color();
-    $darck   = darck();
-    $light   = light();
-    $blanc   = blanc();
-
 ?>
 
 * {
-    outline-color: <?php echo $light ?>; 
+    outline-color: <?php echo light() ?>;
 }
 
 a:focus,
 a:hover {
-    color: <?php echo $default ?>;
+    color: <?php echo color() ?>;
 }
 
 a:active {
-    color: <?php echo $darck ?>; 
+    color: <?php echo darck() ?>;
 }
 
 input[type=submit],
 input[type=reset],
 input[type=button] {
-    background-color: <?php echo $default ?>;
-    box-shadow: 0px 0px 3px <?php echo $default ?>;
-    border: 1px solid <?php echo $default ?>;
+    background-color: <?php echo color() ?>;
+    box-shadow: 0px 0px 3px <?php echo color() ?>;
+    border: 1px solid <?php echo color() ?>;
 }
 
 input[type=submit]:hover,
@@ -82,56 +81,61 @@ input[type=button]:hover,
 input[type=submit]:focus,
 input[type=reset]:focus,
 input[type=button]:focus {
-    border-color: <?php echo $darck ?>;
+    border-color: <?php echo darck() ?>;
 }
 
 div#nav {
-    background-color: <?php echo $default ?>;
-    box-shadow: 0px 0px 7px <?php echo $default ?>;
+    background-color: <?php echo color() ?>;
+    box-shadow: 0px 0px 7px <?php echo color() ?>;
 }
 
 div#nav:hover,
 div#nav:focus {
-    box-shadow: 0px 0px 10px <?php echo $default ?>;
+    box-shadow: 0px 0px 10px <?php echo color() ?>;
 }
 
 div#nav a:hover,
 div#nav a:focus
 {
-    background-color: <?php echo $darck ?>;
+    background-color: <?php echo darck() ?>;
 }
 
 div#pop h3 {
-    background-color: <?php echo $default ?>;
+    background-color: <?php echo color() ?>;
 }
 
 div#perso {
-    background-color: <?php echo $default ?>;
-    box-shadow: 0px 0px 7px <?php echo $default ?>;
+    background-color: <?php echo color() ?>;
+    box-shadow: 0px 0px 7px <?php echo color() ?>;
 }
 
 div#carnet>div:last-of-type a.selected,
 div#messagerie>div:last-of-type a.selected {
-    border-right: solid 3px <?php echo $darck ?>;
+    border-right: solid 3px <?php echo darck() ?>;
 }
 
 div#messagerie>div:last-of-type a.unread  {
-    border-right: dotted 3px <?php echo $light ?>;
+    border-right: dotted 3px <?php echo light() ?>;
 }
 
 div#carnet>div:last-of-type a:hover, 
 div#carnet>div:last-of-type a:focus,
 div#messagerie>div:last-of-type a:hover, 
 div#messagerie>div:last-of-type a:focus  {
-    border-right: solid 3px <?php echo $light ?>;
+    border-right: solid 3px <?php echo light() ?>;
 }
 
 div#param input:focus,
 div#param select:focus {
-    border: 1px solid <?php echo $light ?>;
-    box-shadow: 0px 0px 3px <?php echo $light ?>;
+    border: 1px solid <?php echo light() ?>;
+    box-shadow: 0px 0px 3px <?php echo light() ?>;
 }
 
 div#messagerie span.perso {
-    color: <?php echo $default ?>;
+    color: <?php echo color() ?>;
+}
+
+
+div#perso span.label {
+    color: <?php echo darck2() ?>;
 }
